5c7ff4f19a9cd1fb2820a44860c275052d4665b9
Fabi.exe <66797305+FabiPunktExe@users.noreply.github.com>
2025-11-23 10:58:33 +0100

    Cleanup settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,12 +6,3 @@ pluginManagement {
 }
 
 rootProject.name = "ClientSpoofer"
-
-include("common")
-include("fabric:fabric-1.20.1")
-include("fabric:fabric-1.20.4")
-include("fabric:fabric-1.20.6")
-include("fabric:fabric-1.21.1")
-include("fabric:fabric-1.21.2")
-include("fabric:fabric-1.21.3")
-include("fabric:fabric-1.21.4")
